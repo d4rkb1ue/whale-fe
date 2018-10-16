7e94190a73d9c6a0340d05decac16c42bd0697e5
d4rkb1ue <7874364@gmail.com>
2018-10-15 18:39:13 -0700

    chore: script run build

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+npm run build
 rsync -a -e "ssh -i sheng-macbook.pem" ../build ubuntu@54.183.210.91:/home/ubuntu/whale-fe
 rsync -a -e "ssh -i sheng-macbook.pem" ./whale-fe.service ubuntu@54.183.210.91:/home/ubuntu/
 ssh -i sheng-macbook.pem ubuntu@54.183.210.91 'sudo cp /home/ubuntu/whale-fe.service /etc/systemd/system && sudo systemctl reenable whale-fe.service && sudo systemctl restart whale-fe.service'
