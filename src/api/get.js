4fd8272c591c82bc91c8e7273f9b6508a94354b7
d4rkb1ue <7874364@gmail.com>
2018-11-11 23:46:36 -0800

    feat: standard API query generator

--- src/api/get.js
+++ src/api/get.js
@@ -1,8 +1,95 @@
 import axios from 'axios';
 import { OFFER_API } from '../constants/config'
 
-export const getDemoData = async () => {
-    let res = await axios.get(OFFER_API + '?limit=10')
+const GENERAL_PARA = {
+    offset: true,
+    limit: true,
+    list_id: true,
+}
+
+export const EMPTY_QUERY = {
+    offset: NaN,
+    limit: NaN,
+    list_id: '',
+    annual_refresh: NaN,
+    level: NaN,
+    year: NaN,
+    base_salary: NaN,
+    relocation_fee: NaN,
+    sign_bonus: NaN,
+    post_title: '',
+    satisfaction: '',
+    area: '',
+    degree: '',
+    equity: '',
+    equity_schedule: '',
+    experience: '',
+    experience_level: '',
+    greencard: '',
+    group: '',
+    job_function: '',
+    job_type: '',
+    title: '',
+    interest_point: '',
+    other_offer: '',
+    position_type: '',
+    post_time: null,
+    spider_time: null,
+    promotion_package: '',
+    season: '',
+    url: '',
+    yearly_bonus: '',
+    apply_source: '',
+}
+
+export const makeQuery = (obj) => {
+    if (!obj) { return '' }
+    let query = '?'
+    for (let key in obj) {
+        if (!(key in EMPTY_QUERY)) {
+            console.error('invalid arg', key)
+        }
+        if (Number.isNaN(obj[key]) || !obj[key]) {
+            continue
+        }
+        if (key in GENERAL_PARA) {
+            query += (key + '=' + obj[key])
+        } else {
+            query += ('by_' + key + '=' + obj[key])
+        }
+        query += '&'
+    }
+    if (query === '?') {
+        return ''
+    }
+
+    if (query[query.length - 1] === '&') {
+        query = query.substring(0, query.length - 1)
+    }
+    
+    return query
+}
+
+/**
+ * usage:
+ * makeQuery({
+ *     ...EMPTY_QUERY,
+ *     limit: 10,
+ * })
+ */
+
+export const getData = async (query) => {
+    let res = await axios.get(OFFER_API + query)
     let data = res.data && res.data.offers
     return data || []
-};
+}
+
+export const getDemoData = async () => {
+    let query = makeQuery({
+        ...EMPTY_QUERY,
+        limit: 10,
+    })
+    
+    return await getData(query)
+}
+
